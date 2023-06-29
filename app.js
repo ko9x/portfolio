40a15a15ed77f950499ee552581cce826950405a
Sean McGillis <slcidevice@yahoo.com>
2023-06-28 22:02:58 -0600

    Created an app instance for each section of the project

--- app.js
+++ app.js
@@ -1,13 +1,45 @@
-const app = Vue.createApp({
-    data() {
-        return {
-            listItemText: 'Hello from the list item'
-        };
-    },
-    methods: {
-        myFunction() {
-            return this.listItemText;
-        }
-    }
+const app1 = Vue.createApp({
+  data() {
+    return {
+      headerText: "This is the header section",
+    };
+  },
+  methods: {},
 });
-app.mount('#itemList')
+app1.mount("header");
+const app2 = Vue.createApp({
+  data() {
+    return {
+      projectText: "Here is where the projects section will go",
+    };
+  },
+  methods: {},
+});
+app2.mount("#projects");
+const app3 = Vue.createApp({
+  data() {
+    return {
+      skillsText: "Here is where the skills section will go",
+    };
+  },
+  methods: {},
+});
+app3.mount("#skills");
+const app4 = Vue.createApp({
+  data() {
+    return {
+      aboutText: "Here is where the about section will go",
+    };
+  },
+  methods: {},
+});
+app4.mount("#about");
+const app5 = Vue.createApp({
+  data() {
+    return {
+      contactText: "Here is where the contact form will go",
+    };
+  },
+  methods: {},
+});
+app5.mount("#contact");
